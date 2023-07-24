095baea1d1f66ba86ed2a842301a3d63e4dbbc95
shcube <shandin.va@gmail.com>
2023-07-24 21:44:41 +0700

    remove unused deps

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { CacheModule } from '@nestjs/cache-manager';
-import { MINUTE } from '../../utils/constant/time';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
-        CacheModule.register({
-            // TODO: Redis or Memcached
-            ttl: 5 * MINUTE,
-        }),
         JwtModule.registerAsync({
             useFactory: (configService: ConfigService) => ({
                 secret: configService.get('JWT_SECRET'),
